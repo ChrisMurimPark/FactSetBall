2b8eda23f3d5bb1d34c2f25bd483e5a416cfe553
Chris Park <murim.park@factset.com>
2018-05-17 19:56:28 -0500

    Removing db file, modifying table schemas a bit

--- sqlite_interface.py
+++ sqlite_interface.py
@@ -1,9 +1,24 @@
-import sqlite3
+import datetime
 
 
-def get_high_scores():
-    pass
+def get_high_scores(conn, game_id):
+    cursor = conn.cursor()
+    sql_statement = 'SELECT scores.DatePlayed, scores.User, scores.Score FROM scores ' \
+                    'INNER JOIN games ON scores.GameID = games.ID ' \
+                    'WHERE games.ID = ? ORDER BY Score DESC LIMIT 10'
+    scores = cursor.execute(sql_statement, (game_id,)).fetchall()
+    return scores
 
 
-def record_score(user_id, score, game_id):
-    pass
+def add_score(conn, user, game_id, score):
+    cursor = conn.cursor()
+    sql_statement = 'INSERT INTO scores (DatePlayed, User, GameID, Score) VALUES (?, ?, ?, ?)'
+    cursor.execute(sql_statement, (datetime.datetime.now(), user, game_id, score))
+    conn.commit()
+
+
+def create_tables(conn):
+    cursor = conn.cursor()
+    cursor.execute('CREATE TABLE scores (DatePlayed DATETIME, User CHARACTER(3), GameID INTEGER, Score INTEGER)')
+    cursor.execute('CREATE TABLE games (ID INTEGER, Name TEXT)')
+    conn.commit()
